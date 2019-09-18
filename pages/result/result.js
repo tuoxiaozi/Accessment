6bd5ad91e11787d3af1fc8384aeb92f018be68f3
tuoxiaozi <tuoxiaozi@qq.com>
2019-09-18 10:02:23 +0800

    fix bug finish

--- pages/result/result.js
+++ pages/result/result.js
@@ -12,17 +12,18 @@ Page({
   onLoad(options) {
     const conclusion = options.conclusion
     const conclusionTxt = options.conclusionTxt
+    // console.log(resultLst)
     
-    // const conclusion = '1,2,10'
-    // const conclusionTxt = '中国、美国、日本'
-    var conclusions = conclusion.split(",").map(function (item) {
-      return item > 9? item-9:item;
-    });
+    // const conclusion = '9,11'
+    // const conclusionTxt = '中国、美国'
+    var conclusions = conclusion.split(",").map(item => {
+      return (item > 9? item-9:item).toString()
+    })
     console.log(conclusions);
     this.setData({
       conclusions: conclusions,
       conclusionTxts: conclusionTxt.split("、"),
-      resultLst: resultLst
+      resultLst
     })
     // console.log('conclusions----------->',this.data.conclusions,'conclusionTxts--------->',this.data.conclusionTxts,'resultLst----->',this.data.resultLst)
   },
@@ -47,7 +48,6 @@ Page({
 
   reTest() {
     my.redirectTo({
-      // url: `/pages/result/result&id=${this.data.resId}`
       url: `/pages/medical/medical`
     })
   },
